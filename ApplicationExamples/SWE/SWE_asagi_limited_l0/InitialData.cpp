b811837e19180e9d3363fe53666263ab50a1e83e
Adam Greenbank <adam.r.greenbank@gmail.com>
2022-04-23 17:56:18 +0100

    fixed typo

--- ApplicationExamples/SWE/SWE_asagi_limited_l0/InitialData.cpp
+++ ApplicationExamples/SWE/SWE_asagi_limited_l0/InitialData.cpp
@@ -93,8 +93,8 @@ void InitialData::readAsagiData(const double* const x,double* Q){
 	query.x(0,2)=0;
 	model->evaluate(query,adapter);
 	
-	Q[3]= bathymetry[0]
-
+	Q[3]= bathymetry[0];
+	
 	easi::ArraysAdapter<double> adapter2;
 	adapter2.addBindingPoint("d",displacement);
 
@@ -109,7 +109,7 @@ void InitialData::readAsagiData(const double* const x,double* Q){
 		std::cout << "Error when reading bathymetry" << x[0] << "," << x[1] << std::endl;
 	Q[1]= 0;
 	Q[2]= 0;
-	Q[3]+= displacement[0]; 
+	Q[3]+= displacement[0];
 }
 
 void InitialData::getInitialData(const double* const x,double* Q) {
