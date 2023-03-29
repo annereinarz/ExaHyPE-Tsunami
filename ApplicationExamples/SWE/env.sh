df560a7e913b4976597337133ff655e6aee73797
reinarz <anne.k.reinarz@durham.ac.uk>
2023-03-29 08:20:25 +0100

    correct environment script

--- ApplicationExamples/SWE/env.sh
+++ ApplicationExamples/SWE/env.sh
@@ -3,13 +3,14 @@ cd /ExaHyPE-Tsunami/ApplicationExamples/SWE/SWE_asagi_limited_l0 && cp exahype_d
     cd /ExaHyPE-Tsunami/ApplicationExamples/SWE/SWE_asagi_limited_l2 && cp exahype_debug.log-filter exahype.log-filter
 
 threads=28
+consumers=27
 ranks=1
-cd /ExaHyPE-Tsunami/ApplicationExamples/SWE && sed -i 's/\"cores\": .*/\"cores\": $threads,/g' SWE_asagi_limited_l0.exahype2
-cd /ExaHyPE-Tsunami/ApplicationExamples/SWE && sed -i 's/\"cores\": .*/\"cores\": $threads,/g' SWE_asagi_limited_l1.exahype2
-cd /ExaHyPE-Tsunami/ApplicationExamples/SWE && sed -i 's/\"cores\": .*/\"cores\": $threads,/g' SWE_asagi_limited_l2.exahype2
-cd /ExaHyPE-Tsunami/ApplicationExamples/SWE && sed -i 's/\"background_job_consumers\": .*/\"background_job_consumers\": $threads-1/g' SWE_asagi_limited_l0.exahype2
-cd /ExaHyPE-Tsunami/ApplicationExamples/SWE && sed -i 's/\"background_job_consumers\": .*/\"background_job_consumers\": $threads-1/g' SWE_asagi_limited_l1.exahype2
-cd /ExaHyPE-Tsunami/ApplicationExamples/SWE && sed -i 's/\"background_job_consumers\": .*/\"background_job_consumers\": $threads-1/g' SWE_asagi_limited_l2.exahype2
+cd /ExaHyPE-Tsunami/ApplicationExamples/SWE && sed -i "s/\"cores\": .*/\"cores\": $threads,/g" SWE_asagi_limited_l0.exahype2
+cd /ExaHyPE-Tsunami/ApplicationExamples/SWE && sed -i "s/\"cores\": .*/\"cores\": $threads,/g" SWE_asagi_limited_l1.exahype2
+cd /ExaHyPE-Tsunami/ApplicationExamples/SWE && sed -i "s/\"cores\": .*/\"cores\": $threads,/g" SWE_asagi_limited_l2.exahype2
+cd /ExaHyPE-Tsunami/ApplicationExamples/SWE && sed -i "s/\"background_job_consumers\": .*/\"background_job_consumers\": $consumers/g" SWE_asagi_limited_l0.exahype2
+cd /ExaHyPE-Tsunami/ApplicationExamples/SWE && sed -i "s/\"background_job_consumers\": .*/\"background_job_consumers\": $consumers/g" SWE_asagi_limited_l1.exahype2
+cd /ExaHyPE-Tsunami/ApplicationExamples/SWE && sed -i "s/\"background_job_consumers\": .*/\"background_job_consumers\": $consumers/g" SWE_asagi_limited_l2.exahype2
 
 cd /ExaHyPE-Tsunami/ApplicationExamples/SWE/SWE_asagi_limited_l0 && cp exahype_debug.log-filter exahype.log-filter
 cd /ExaHyPE-Tsunami/ApplicationExamples/SWE/SWE_asagi_limited_l1 && cp exahype_debug.log-filter exahype.log-filter
@@ -22,5 +23,5 @@ cd /ExaHyPE-Tsunami/ApplicationExamples/SWE && sed -i 's/\"time\": 1.0,/\"time\"
 cd /ExaHyPE-Tsunami/ApplicationExamples/SWE && sed -i 's/\"time\": 1.0,/\"time\": 10000.0,/g' SWE_asagi_limited_l1.exahype2
 cd /ExaHyPE-Tsunami/ApplicationExamples/SWE && sed -i 's/\"time\": 1.0,/\"time\": 10000.0,/g' SWE_asagi_limited_l2.exahype2
 
-mpirun --allow-run-as-root -x OMP_NUM_THREADS=$threads -map-by numa:pe=28 -x LD_LIBRARY_PATH -x SHARED_DIR -n ranks ./ExaHyPE-SWE ../SWE_asagi_limited_l2.exahype2
+mpirun --allow-run-as-root -x OMP_NUM_THREADS=$threads -map-by numa:pe=28 -x LD_LIBRARY_PATH -x SHARED_DIR -n $ranks ./ExaHyPE-SWE ../SWE_asagi_limited_l2.exahype2
 
