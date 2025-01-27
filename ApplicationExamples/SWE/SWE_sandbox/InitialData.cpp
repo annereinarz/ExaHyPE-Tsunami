cb2ed0df4e24604d4bf775fd4bf53ae67c749eec
Anne Reinarz <12151628+annereinarz@users.noreply.github.com>
2025-01-27 12:49:08 +0000

    Update InitialData.cpp

--- ApplicationExamples/SWE/SWE_sandbox/InitialData.cpp
+++ ApplicationExamples/SWE/SWE_sandbox/InitialData.cpp
@@ -71,12 +71,15 @@ void InitialData::readAsagiData(const double* const x,double* Q){
 	query.x(0,2)=0;
 	model->evaluate(query,adapter);
 
-	Q[0]=std::max(0.0,bathymetry[0]);
-	if(std::isnan(Q[0]))
-		std::cout << "Error when reading bathymetry" << x[0] << "," << x[1] << std::endl;
-	Q[1]= 0;
-	Q[2]= 0;
-	Q[3]= 100 - bathymetry[0]; //TODO 
+	double sigma =  30.0; // width of initial wave
+        double x_center = 400;
+        double y_center = 300;
+        Q[0]= 0.1 * exp(-((x[0] - x_center)*(x[0] - x_center) + (x[1] - y_center)*(x[1] - y_center)) / (2 * sigma*sigma)) - bathymetry[0];
+        if(std::isnan(Q[0]))
+                std::cout << "Error when reading bathymetry" << x[0] << "," << x[1] << std::endl;
+        Q[1]= 0;
+        Q[2]= 0;
+        Q[3]= bathymetry[0];
 }
 
 void InitialData::getInitialData(const double* const x,double* Q) {
